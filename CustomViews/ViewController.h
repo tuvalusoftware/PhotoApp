bdd85cdcb9fe87497157408a140e0e470d43cd36
TouchROI <touchroi@TouchROIs-MacBook-Pro.local>
2014-02-27 14:51:57 -0500

    Initial Commit

---
+++ CustomViews/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomViews
+//
+//  Created by TouchROI on 2/27/14.
+//  Copyright (c) 2014 TouchROI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CustomViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomViews
+//
+//  Created by TouchROI on 2/27/14.
+//  Copyright (c) 2014 TouchROI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
